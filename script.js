72a1a116df5bc48e11e49f57b18c90b58dc458e7
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-01 19:41:32 -0400

    Update album section

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
+const button = document.querySelector("#myButton");
+
+button.addEventListener("click", function() {
+  // Code to be executed when the button is clicked
+});
